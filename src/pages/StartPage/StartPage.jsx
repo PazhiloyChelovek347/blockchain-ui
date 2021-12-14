61f09f1a2ad8218015a60f960062573a8f0c883d
Semyon Suhorada <s.suhorada@dunice.net>
2021-12-14 11:15:41 +0300

    syling

--- src/pages/StartPage/StartPage.jsx
+++ src/pages/StartPage/StartPage.jsx
@@ -1,8 +1,5 @@
-import { Card, Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
 import React, { memo, useState } from "react";
-import { FormGroup, TextField, Button } from "@mui/material";
-import { useDispatch } from "react-redux";
-import { signin } from "../../redux/actionCreators/basicActions";
 
 const StartPage = ({ contract, web3 }) => {
   const [logs, setLogs] = useState([]);
@@ -10,8 +7,6 @@ const StartPage = ({ contract, web3 }) => {
   const [chen, setChen] = useState({});
   const [lastBooking, setBooking] = useState({});
 
-
-  // const dispatch = useDispatch();
   const u1 = async () => {
     contract.methods
       .createUser("0xCfaA18F0616309874573dc4b5c9eDcf346E22895", "chen", "1pos")
@@ -19,7 +14,12 @@ const StartPage = ({ contract, web3 }) => {
     const u = await contract.methods
       .getUser("0xCfaA18F0616309874573dc4b5c9eDcf346E22895")
       .call();
-    setInv({ role: u[4], balance: web3.utils.fromWei((await web3.eth.getBalance('0xCfaA18F0616309874573dc4b5c9eDcf346E22895'))) });
+    setInv({
+      role: u[4],
+      balance: web3.utils.fromWei(
+        await web3.eth.getBalance("0xCfaA18F0616309874573dc4b5c9eDcf346E22895")
+      ),
+    });
   };
 
   const u2 = async () => {
@@ -33,7 +33,12 @@ const StartPage = ({ contract, web3 }) => {
     const u = await contract.methods
       .getUser("0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd")
       .call();
-    setChen({ role: u[4], balance: web3.utils.fromWei((await web3.eth.getBalance('0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd'))) });
+    setChen({
+      role: u[4],
+      balance: web3.utils.fromWei(
+        await web3.eth.getBalance("0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd")
+      ),
+    });
   };
 
   const gi = () => {
@@ -49,22 +54,32 @@ const StartPage = ({ contract, web3 }) => {
     await contract.methods
       .setRole("0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd", 1)
       .send({ from: "0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd" });
-      await contract.methods
+    await contract.methods
       .createBook(
         "2021.21.12",
         "0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd",
         "0xCfaA18F0616309874573dc4b5c9eDcf346E22895"
-        )
-        .send({ from: "0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd" });
-        const u1 = await contract.methods
-        .getUser("0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd")
-        .call();
-      setInv({ role: u1[4], balance: web3.utils.fromWei((await web3.eth.getBalance('0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd'))) });
-      const u2 = await contract.methods
-        .getUser("0xCfaA18F0616309874573dc4b5c9eDcf346E22895")
-        .call();
-      setChen({ role: u2[4], balance: web3.utils.fromWei((await web3.eth.getBalance('0xCfaA18F0616309874573dc4b5c9eDcf346E22895'))) });
-    setBooking((await contract.methods.getBookings().call()));
+      )
+      .send({ from: "0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd" });
+    const u1 = await contract.methods
+      .getUser("0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd")
+      .call();
+    setInv({
+      role: u1[4],
+      balance: web3.utils.fromWei(
+        await web3.eth.getBalance("0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd")
+      ),
+    });
+    const u2 = await contract.methods
+      .getUser("0xCfaA18F0616309874573dc4b5c9eDcf346E22895")
+      .call();
+    setChen({
+      role: u2[4],
+      balance: web3.utils.fromWei(
+        await web3.eth.getBalance("0xCfaA18F0616309874573dc4b5c9eDcf346E22895")
+      ),
+    });
+    setBooking(await contract.methods.getBookings().call());
     await web3.eth.sendTransaction({
       from: "0xCfaA18F0616309874573dc4b5c9eDcf346E22895",
       to: "0xDCcfE0E4782340Cb93e21BA4e4774991377Ee6Dd",
@@ -100,7 +115,6 @@ const StartPage = ({ contract, web3 }) => {
         <Button type="submit" value="submit" onClick={gi} variant="contained">
           get invoker
         </Button>
-
       </div>
       <div
         style={{
@@ -112,9 +126,15 @@ const StartPage = ({ contract, web3 }) => {
         }}
       >
         <Typography>Logs:</Typography>
-        <Typography style={{wordBreak: 'break-all'}}>{`chen role ${inv.role},money ${inv.balance}`}</Typography>
-        <Typography style={{wordBreak: 'break-all'}}>{`invoker role ${chen.role},money ${chen.balance}`}</Typography>
-        <Typography style={{wordBreak: 'break-all'}}>{`booking: ${lastBooking}`}</Typography>
+        <Typography
+          style={{ wordBreak: "break-all" }}
+        >{`chen role ${inv.role},money ${inv.balance}`}</Typography>
+        <Typography
+          style={{ wordBreak: "break-all" }}
+        >{`invoker role ${chen.role},money ${chen.balance}`}</Typography>
+        <Typography
+          style={{ wordBreak: "break-all" }}
+        >{`booking: ${lastBooking[0][0]}`}</Typography>
       </div>
     </>
   );
